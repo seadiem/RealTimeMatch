6ab5eb792358bb57e4ea424823a1b4cfffccdeae
NKH 27 <nkh27@iMac-NKH-3.local>
2017-01-26 19:54:26 +0300

    translate work

--- TestMatchmaker/Shape.swift
+++ TestMatchmaker/Shape.swift
@@ -10,9 +10,10 @@ extension Circle: Drawable {
     func draw() {
         let ctx = UIGraphicsGetCurrentContext()
         ctx?.saveGState()
+        ctx?.translateBy(x: location.x, y: location.y)
         ctx?.setStrokeColor(UIColor.green.cgColor)
         ctx?.setLineWidth(10)
-        ctx?.addArc(center: location, radius: 10, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: false)
+        ctx?.addArc(center: CGPoint.zero, radius: 10, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: false)
         ctx?.strokePath()
         ctx?.restoreGState()
     }
